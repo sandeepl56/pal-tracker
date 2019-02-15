9f7c70bf155050d4081e6a0b83a9a5cfe74e2eb5
Sandeepkumar L (Securities) <SA296118@wipro.com>
2019-02-15 13:18:05 +0530

    new database

--- databases/tracker/create_databases.sql
+++ databases/tracker/create_databases.sql
@@ -1,11 +1,11 @@
-DROP DATABASE IF EXISTS tracker_dev;
-DROP DATABASE IF EXISTS tracker_test;
+DROP DATABASE IF EXISTS tracker_dev1;
+DROP DATABASE IF EXISTS tracker_test1;
 
-CREATE DATABASE tracker_dev;
-CREATE DATABASE tracker_test;
+CREATE DATABASE tracker_dev1;
+CREATE DATABASE tracker_test1;
 
 CREATE USER IF NOT EXISTS 'tracker'@'localhost'
   IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'localhost';
-GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'localhost';
+GRANT ALL PRIVILEGES ON tracker_dev1.* TO 'tracker' @'localhost';
+GRANT ALL PRIVILEGES ON tracker_test1.* TO 'tracker' @'localhost';
 
