662f424643694ee54f5652a686b9c9603e3bf76e
Sandeepkumar L (Securities) <SA296118@wipro.com>
2019-02-18 11:00:01 +0530

    controller test update

--- src/test/java/test/pivotal/pal/tracker/TimeEntryControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/TimeEntryControllerTest.java
@@ -1,5 +1,7 @@
 package test.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.pivotal.pal.tracker.TimeEntry;
 import io.pivotal.pal.tracker.TimeEntryController;
@@ -21,12 +23,20 @@ import static org.mockito.Mockito.*;
 public class TimeEntryControllerTest {
     private TimeEntryRepository timeEntryRepository;
     private TimeEntryController controller;
-    private  MeterRegistry meterRegistry;
+
 
     @Before
     public void setUp() throws Exception {
         timeEntryRepository = mock(TimeEntryRepository.class);
-        meterRegistry = mock(MeterRegistry.class);
+        MeterRegistry meterRegistry = mock(MeterRegistry.class);
+        doReturn(mock(DistributionSummary.class))
+                .when(meterRegistry)
+                .summary("timeEntry.summary");
+
+        doReturn(mock(Counter.class))
+                .when(meterRegistry)
+                .counter("timeEntry.actionCounter");
+
         controller = new TimeEntryController(timeEntryRepository, meterRegistry);
     }
 
